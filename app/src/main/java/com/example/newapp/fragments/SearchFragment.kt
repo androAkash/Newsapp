c0f9cf1951682a468dba0573c2ad9d3c1af7bbb4
Akash <akashbhattacharyak1314@gmail.com>
2023-02-08 19:34:35 +0530

    search implementation done

--- app/src/main/java/com/example/newapp/fragments/SearchFragment.kt
+++ app/src/main/java/com/example/newapp/fragments/SearchFragment.kt
@@ -5,17 +5,116 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
+import androidx.paging.LoadState
+import androidx.paging.PagingData
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.newapp.R
+import com.example.newapp.adapter.NewsLoadStateAdapter
+import com.example.newapp.adapter.PagingNewsAdapter
+import com.example.newapp.databinding.FragmentSearchBinding
+import com.example.newapp.model.Article
+import com.example.newapp.viewModel.NewViewModel
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 
 class SearchFragment : Fragment() {
-
+    private val viewModel: NewViewModel by activityViewModels()
+    private lateinit var binding: FragmentSearchBinding
+    private lateinit var pagingNewsAdapter: PagingNewsAdapter
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        binding = FragmentSearchBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupRecyclerView()
+
+        binding.btRetry.setOnClickListener { pagingNewsAdapter.retry() }
+
+        binding.svSearch.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                query?.let {
+                    viewModel.getSearchNews(query)
+                }
+                binding.svSearch.clearFocus()
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
+            }
+        })
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED){
+                viewModel.searchNews.collectLatest { articles ->
+                    pagingNewsAdapter.apply {
+                        submitData(PagingData.empty())
+                        submitData(articles)
+                    }
+                }
+            }
+        }
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            lifecycleScope.launch {
+                pagingNewsAdapter.loadStateFlow.collectLatest { newsLoadState ->
+                    when (newsLoadState.source.refresh) {
+                        is LoadState.Error -> {
+                            binding.apply {
+                                progressBar.visibility = View.INVISIBLE
+                                btRetry.visibility = View.VISIBLE
+                                tvError.visibility = View.VISIBLE
+                                rvSearchNews.visibility = View.INVISIBLE
+                            }
+                        }
+                        is LoadState.Loading -> {
+                            binding.apply {
+                                rvSearchNews.visibility = View.INVISIBLE
+                                progressBar.visibility = View.VISIBLE
+                            }
+                        }
+                        is LoadState.NotLoading -> {
+                            binding.apply {
+                                progressBar.visibility = View.INVISIBLE
+                                btRetry.visibility = View.INVISIBLE
+                                tvError.visibility = View.INVISIBLE
+                                rvSearchNews.visibility = View.VISIBLE
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun setupRecyclerView() {
+        pagingNewsAdapter = PagingNewsAdapter{ article ->
+            onClick(article)
+        }
+        binding.rvSearchNews.apply {
+            adapter = pagingNewsAdapter.withLoadStateFooter(NewsLoadStateAdapter())
+            layoutManager = LinearLayoutManager(activity)
+        }
+    }
+
+    private fun onClick(article: Article){
+        val bundle = Bundle().apply {
+            putSerializable("article", article)
+        }
     }
 
 }
